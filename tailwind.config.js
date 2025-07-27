4d30b6ed4bf1f7cb2db1af46de796f813125f2e4
bejar-dev <bejarramosb@gmail.com>
2025-07-26 20:03:10 -0500

    Animaciones con CSS Puro y TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        flotar: {
+          '0%, 100%': {transform: 'translateY(0)'},
+          '50%': {transform: 'translateY(-10px)'}
+        }
+      },
+      animation: {
+        flotar: 'flotar 2s ease-in-out infinite',
+      }
+    },
   },
   plugins: [],
 }
